04b795db87b5c5521e73312d81367bf2c510f36c
jsantiag <jsantiag@wellesley.edu>
2019-01-15 11:40:23 -0600

    added auth, utils, models in progress, mlab linked, dependencies updated

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const passport = require('passport');
+const localStrategy = require('./passport/local');
+const jwtStrategy = require('./passport/jwt'); 
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
-// const {dbConnect} = require('./db-knex');
 
 const app = express();
 
