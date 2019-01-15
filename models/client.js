51727717902b1794882f6934f3b075d3f621a7b4
jsantiag <jsantiag@wellesley.edu>
2019-01-15 14:22:36 -0600

    updated schemas

---
+++ models/client.js
@@ -0,0 +1,35 @@
+'use strict'; 
+
+const mongoose = require('mongoose'); 
+
+const ClientSchema = mongoose.Schema({
+  _id: {
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User'
+  },
+  company: {
+    type:String, 
+    required:true
+  },
+  hourlyRate:Number,
+  phoneNumber:String,
+  invoice:[
+    {
+      month:Number, 
+      year:Number, 
+      paid:Boolean
+    }
+  ]
+}); 
+
+ClientSchema.set('toJSON', {
+  virtuals: true, 
+  transform: (doc, result) => {
+    delete result._id; 
+    delete result.__v; 
+  }
+}); 
+
+const Client = mongoose.model('Client', ClientSchema); 
+
+module.exports = {Client}; 
---
+++ models/user.js
@@ -0,0 +1,52 @@
+'use strict'; 
+
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose'); 
+
+mongoose.Promise = global.Promise; 
+
+const UserSchema = mongoose.Schema({
+  _id:{type:mongoose.Schema.Types.ObjectId},
+  email:{
+    type:String,
+    required:true,
+    unique:true
+  },
+  organizationName:{type:String, required:true},
+  organizationPhoneNumber:String,
+  globalHourlyRate:Number,
+  password:{type:String, required: true}, 
+  twilio: {
+    authToken:{type:String, required:true}, 
+    dateCreated:{type:Date},
+    dateUpdated:{type:Date}, 
+    friendlyName:{type:String, required:true},
+    sid:{type:String},
+    status:{type:String},
+    phones:[{
+      sid:{
+        friendlyName:{type:String}, number:Number }
+    }]
+  }
+});
+
+UserSchema.methods.serialize = function(){
+  return {
+    email:this.email || '',
+    organizationName: this.organizationName || '', 
+    organizationPhoneNumber: this.organizationPhoneNumber || ''
+  };
+};
+
+UserSchema.methods.validatePassword = function(password) {
+  return bcrypt.compare(password, this.password);
+}; 
+
+UserSchema.statics.hashPassword = function(password) {
+  return bcrypt.hash(password, 10); 
+}; 
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = {User}; 
+
