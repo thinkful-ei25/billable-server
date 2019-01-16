fe3f2eb90c95d0b07547e0a7572a7d2085a19f77
Sean Nealon <signalflowsean@gmail.com>
2019-01-16 17:48:07 -0500

    simplified getting an account

--- routes/account.js
+++ routes/account.js
@@ -5,27 +5,20 @@ const { CLIENT } = require('../config');
 const User = require('../models/user'); 
 const validateUser = require('../utils/validateUser'); 
 
-//See Twilio status
 router.get('/user', (req, res) => { 
   console.log('GET A PRE-EXISTING SUBACCOUNT / USER'); 
+  const { sid } = req.user.twilio;
 
-  // const { id } = req.body.id; 
-  let tempId = '5c3fae2b152d093f16f1e236'; 
-  return User.findById( tempId )
-    .then(account => {
-      let sid = account.twilio.sid; 
-      console.log('sid', sid); 
-      CLIENT.api.accounts(sid)
-      .fetch()
-      .then(account => { 
-        console.log(account); 
-        res.json(account); 
-      })
-      .catch(err => { 
-        console.log('err', err); 
-      })
-      .done();
-    }); 
+  CLIENT.api.accounts(sid)
+  .fetch()
+  .then(account => { 
+    console.log(account); 
+    res.json(account); 
+  })
+  .catch(err => { 
+    console.log('err', err); 
+  })
+  .done();
 }); 
 
 router.delete('/user', (req, res) => { 
