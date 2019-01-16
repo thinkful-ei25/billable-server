f69888584def5d675b020e5cfa99b79ebae5baff
Box <40639989+theBoxed@users.noreply.github.com>
2019-01-16 11:07:53 -0500

    config.js

--- config.js
+++ config.js
@@ -15,8 +15,8 @@ module.exports = {
   TEST_TWILIO_AUTH_TOKEN: process.env.TEST_TWILIO_AUTH_TOKEN, 
   TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID, 
   TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
-  CLIENT: require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN), 
-  TEST_CLIENT: require('twilio')(process.env.TEST_TWILIO_ACCOUNT_SID, process.env.TEST_TWILIO_AUTH_TOKEN), 
+  CLIENT: twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN), 
+      TEST_CLIENT: twilio(process.env.TEST_TWILIO_ACCOUNT_SID, process.env.TEST_TWILIO_AUTH_TOKEN), 
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
 };
