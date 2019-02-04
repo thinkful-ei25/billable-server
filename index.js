f2762d3493658682c5ead9bca10d43026bc488cc
Jonathon Garrett <Reldalk@gmail.com>
2019-02-04 13:35:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ app.use(
   })
 );
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
+//Delete this if local
+app.use(express.static('client/build'));
+
 
 app.get('*', (request, response) => {
     response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
