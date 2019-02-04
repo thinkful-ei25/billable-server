bb270667a68933b78b64acc35c2880fbb01a1b47
Jonathon Garrett <Reldalk@gmail.com>
2019-02-04 16:00:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,15 +27,12 @@ app.use(
   })
 );
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  if (req.method === 'OPTIONS') {
-    return res.send(204);
-  }
-  next();
-});
+app.use(
+  cors({
+  origin: CLIENT_ORIGIN,
+  'https://contactmeter.herokuapp.com'
+  })
+);
 
 app.use(express.json());
 
