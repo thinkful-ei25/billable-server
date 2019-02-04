42d860b4731dcebf464db380e1460e113775e108
Jonathon Garrett <Reldalk@gmail.com>
2019-02-04 15:55:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,11 +27,15 @@ app.use(
   })
 );
 
-app.use(
-  cors({
-    Access-Control-Allow-Origin: *
-  })
-);
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
 
 app.use(express.json());
 
